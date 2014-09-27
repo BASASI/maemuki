11229a35aa4c66de631bf27462f184ae6759840b
izumin5210 <masayuki@izumin.info>
2014-09-27 16:31:48 +0900

    Implement some methods for auth

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,2 +1,18 @@
 class Identity < ActiveRecord::Base
+
+  belongs_to :user
+
+  validates_presence_of :uid, :provider, :token
+  validates_uniqueness_of :provider, scope: :uid
+  validates_uniqueness_of :provider, scope: :user_id
+
+  def self.create_from_omniauth(auth)
+    create!(
+      provider: auth.provider,
+      uid: auth.uid,
+      token: auth.credentials.token,
+      secret: auth.credentials.secret,
+      expires_at: auth.credentials.expires_at
+    )
+  end
 end
