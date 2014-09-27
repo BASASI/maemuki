433623f011dded1b02bfb6e3242e1d50a2f93505
izumin5210 <masayuki@izumin.info>
2014-09-28 05:44:41 +0900

    Add some methods for get user's tweets

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -6,6 +6,29 @@ class Identity < ActiveRecord::Base
   validates_uniqueness_of :provider, scope: :uid
   validates_uniqueness_of :provider, scope: :user_id
 
+  def twitter_client
+    if self.provider == 'twitter'
+      Twitter::REST::Client.new do |config|
+        secrets = Rails.application.secrets
+        config.consumer_key = secrets.twitter['key']
+        config.consumer_secret = secrets.twitter['secret']
+        config.access_token = self.token
+        config.access_token_secret = self.secret
+      end
+    end
+  end
+
+  def statuses(oldest_id = nil)
+    client = twitter_client
+    opts = {count: 200, include_rts: false}
+    # TODO: 最大で180回？
+    (1..5).map { |i|
+      client.user_timeline(oldest_id.nil? ? opts : opts.merge(max_id: oldest_id)).tap do |tweets|
+        oldest_id = tweets.last.id - 1
+      end
+    }.flatten
+  end
+
   def self.create_from_omniauth(auth)
     create!(
       provider: auth.provider,
