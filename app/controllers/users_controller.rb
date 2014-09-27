c91cb184d3144fe1a7bb74d7c9ce17f67c0f0243
やましー <3.1415mailadress@gmail.com>
2014-09-27 19:15:25 +0900

    users_controllerのprivate削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,9 +2,4 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
-
-  private
-      def user_params
-        params.require(:user).permit(:name, :screen_name, :image)
-      end
 end
